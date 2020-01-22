7c27531ec53d8c85cc3959acc066dc361022d5aa
Code4Blessings <robinwarden@code4blessings.com>
2020-01-22 10:52:27 -0500

    Updated db-helpers.js file

--- data/db-helpers.js
+++ data/db-helpers.js
@@ -1,1 +1,18 @@
-const db = require('./db-config')
+const db = require('./db-config');
+
+db.select('*').from('users');
+
+//shortcut
+db('users');
+//resolves to an array of users
+
+db('users').where({ id: 1 });
+//resolves to an array containing any users that match the where.
+
+db('users').insert({ name: 'danny', age: 12 });
+
+db('users').insert({ name: 'robin', age: 44});
+
+db('users').where({ id: 2}).update({ age: 45});
+
+db('users').where ({ id: 2}).del();
